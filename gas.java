86297417b73979ac4799f1fa8706ac03fc2bb58e
Eleanorttt <136740852+Eleanorttt@users.noreply.github.com>
2024-10-10 10:12:07 +0300

    10.10.2024    10:12

--- gas.java
+++ gas.java
@@ -1,5 +1,17 @@
 public class gas {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int gas = 10;
+        int distance = 100;
+        int mpg = 20;
+        for(int i=0;i<gas;i++){
+            if(distance/mpg <= i){
+                System.out.println("You can make it");
+                break;
+            }
+            else{
+                System.out.println("You can't make it");
+                break;
+            }
+        }
     }
 }
